132ee52152c7e84b1bff49545760dfc327d65b3b
Sebastian Schoenherr <seb.schoenherr@gmail.com>
2016-08-18 11:56:00 +0200

    Update r-packages.R

--- conf/r-packages.R
+++ conf/r-packages.R
@@ -1,4 +1,5 @@
 install.packages("knitr", repos = "http://cran.case.edu" );
 install.packages("markdown", repos = "http://cran.case.edu" );
+install.packages("rmarkdown", repos = "http://cran.case.edu" );
 install.packages("ggplot2", repos = "http://cran.case.edu" );
 install.packages("data.table", repos = "http://cran.case.edu" );
